c5abca3c070683e72adb1f0e9a79a44313fdbcee
Mbacke CISSE <206064578+mbackeCISSE@users.noreply.github.com>
2025-05-28 13:26:39 +0000

    first commit for this project

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# odin_Etch-a-Sketch
+# Etch a Sketch
+This project by The Odin Project curruculum foundations of web development aims to incrase my skills in DOM manipulation. Really, It helped me a lot.
+
+An Etch A Sketch has a thick, flat gray screen in a red plastic frame. There are two white knobs on the front of the frame in the lower corners. Twisting the knobs moves a stylus that displaces aluminum powder on the back of the screen, leaving a solid line. The knobs create lineographic images. The left control moves the stylus horizontally, and the right one moves it vertically.
+source: wikipedia.
+## What should I do?
+For this project, I will implement a Etch a Sketch in Javascript. Notice that We have not to create the true Etch a Sketch but a simple large square that content a multiple smallest identic squares depending on dimension and user can hovering them to draw something. hovering a square change its color randomizily.
+## My work
+In this section, I put a couple of screenshoots takes when I a execute the drawer.
+
+* The first to see when we execute the Etch a Sketch(default dimension is 16x16)
+
+![first]()
+
+* A space with 50x50 grid of square.
+
+![second]()
+
+* When we are trying to draw.
+
+![third]()
+
+* Possibility to erase all square(put in white) by click the eraser icon button at righ.
+
+![fourth]()
+
+* when dimension is 100x100.
+
+![fifth]()
+
---
+++ script.js
@@ -0,0 +1,187 @@
+function random(limit) {
+    return Math.floor(Math.random() * limit);
+}
+
+function calculGap(numberOfSquare, dimension) {
+    const totalSquareWide = dimension / numberOfSquare;
+    return totalSquareWide * 0.15;
+}
+let currentDimension = 16;
+let currentSpace = 800;
+let currentGap = calculGap(currentDimension, currentSpace);
+let currentSquareDimension = (currentSpace - (currentGap * currentDimension)) / currentDimension;
+
+let isHovering = false;
+
+function createGridSquare(dimension, space, squareDimension, gap) {
+    const container = document.querySelector('div.container');
+    container.style.width = `${space}px`;
+    container.style.height = `${space}px`;
+    container.style.gap = `${gap}px`;
+    
+    for(let i = 0; i < dimension; i++) {
+        let divTagLine = document.createElement('div');
+        divTagLine.classList.add('line');
+        divTagLine.style.gap = `${gap}px`;
+        for(let j = 0; j < dimension; j++) {
+            let divTagSquare = document.createElement('div');
+            let divTagSquareIn = document.createElement('div');
+
+            divTagSquare.style.width = `${squareDimension}px`;
+            divTagSquare.style.height = `${squareDimension}px`;
+            divTagSquareIn.style.width = `${squareDimension}px`;
+            divTagSquareIn.style.height = `${squareDimension}px`;
+
+            divTagSquare.classList.add('Square');
+            divTagSquareIn.classList.add('square');
+            divTagSquare.appendChild(divTagSquareIn);
+            divTagLine.appendChild(divTagSquare);
+        }
+        container.appendChild(divTagLine);
+    }
+}
+
+createGridSquare(currentDimension, currentSpace, currentSquareDimension, currentGap);
+
+let allSquare = document.querySelectorAll('div.square');
+
+let isClicked = false;
+let isDblClicked = false;
+
+allSquare.forEach(square => {
+    square.addEventListener('mouseover', (e) => {
+        if(isDblClicked === false) {
+            isHovering = true;
+            let opacityValue = getComputedStyle(e.target).opacity;
+            if(Number(opacityValue) !== 0) {
+                e.target.style.opacity = `${Number(opacityValue) - 0.1}`;
+            }
+            e.target.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+            e.target.style.width = `${currentSquareDimension - (currentSquareDimension * 0.2)}px`;
+            e.target.style.height = `${currentSquareDimension - (currentSquareDimension * 0.2)}px`;
+        }
+    })
+})
+
+allSquare.forEach(square => {
+    square.addEventListener('mouseout', (e) => {
+        if(isDblClicked === false) {
+            if(isClicked === false) {
+                e.target.style.width = `${currentSquareDimension}px`;
+                e.target.style.height = `${currentSquareDimension}px`;
+            }
+            else {
+                isClicked = false;
+            }
+        }
+    })
+})
+
+allSquare.forEach(square => {
+    square.addEventListener('click', (e) => {
+        if(isDblClicked === false) {
+            isClicked = true;
+            e.target.style.backgroundColor = 'white';
+            e.target.style.opacity = '1';
+            e.target.style.width = `${currentSquareDimension}px`;
+            e.target.style.height = `${currentSquareDimension}px`;
+        }
+    })
+})
+
+allSquare.forEach(square => {
+    square.addEventListener('dblclick', (e) => {
+        isDblClicked = !isDblClicked;
+    })
+})
+
+// relancement
+
+const buttonValid = document.querySelector('#valid');
+buttonValid.addEventListener('click', () => {
+    const newDimension = document.querySelector('#dimension').value;
+    if(newDimension > 0 && newDimension <= 100 && newDimension != currentDimension) {
+        isDblClicked = false;
+        const lines = document.querySelectorAll('div.line');
+        lines.forEach(line => {
+            line.remove();
+        })
+        currentDimension = newDimension;
+        currentGap = calculGap(currentDimension, currentSpace);
+        currentSquareDimension = (currentSpace - (currentGap * currentDimension)) / currentDimension;
+
+        createGridSquare(currentDimension, currentSpace, currentSquareDimension, currentGap);
+
+        allSquare = document.querySelectorAll('div.square');
+        
+        allSquare.forEach(square => {
+            square.addEventListener('mouseover', (e) => {
+                if(isDblClicked === false) {
+                    isHovering = true;
+                    let opacityValue = getComputedStyle(e.target).opacity;
+                    if(Number(opacityValue) !== 0) {
+                        e.target.style.opacity = `${Number(opacityValue) - 0.1}`;
+                    }
+                    e.target.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+                    e.target.style.width = `${currentSquareDimension - (currentSquareDimension * 0.2)}px`;
+                    e.target.style.height = `${currentSquareDimension - (currentSquareDimension * 0.2)}px`;
+                }
+            })
+        })
+
+        allSquare.forEach(square => {
+            square.addEventListener('mouseout', (e) => {
+                if(isDblClicked === false) {
+                    if(isClicked === false) {
+                        e.target.style.width = `${currentSquareDimension}px`;
+                        e.target.style.height = `${currentSquareDimension}px`;
+                    }
+                    else {
+                        isClicked = false;
+                    }
+                }
+            })
+        })
+
+        allSquare.forEach(square => {
+            square.addEventListener('click', (e) => {
+                if(isDblClicked === false) {
+                    isClicked = true;
+                    e.target.style.backgroundColor = 'white';
+                    e.target.style.opacity = '1';
+                    e.target.style.width = `${currentSquareDimension}px`;
+                    e.target.style.height = `${currentSquareDimension}px`;
+                }
+            })
+        })
+
+        allSquare.forEach(square => {
+            square.addEventListener('dblclick', (e) => {
+                isDblClicked = !isDblClicked;
+            })
+        })
+
+    }
+})
+
+const eraseButton = document.querySelector('.tools button');
+
+eraseButton.addEventListener('click', () => {
+    if(isHovering == true) {
+        allSquare.forEach(square => {
+            square.style.backgroundColor = 'white';
+            square.style.opacity = '1';
+        });
+        isHovering = false;
+    }
+})
+
+eraseButton.addEventListener('mouseover', () => {
+    const text = document.querySelector('.tools button span');
+    text.style.display = 'inline';
+});
+
+eraseButton.addEventListener('mouseout', () => {
+    const text = document.querySelector('.tools button span');
+    text.style.display = 'none';
+});
